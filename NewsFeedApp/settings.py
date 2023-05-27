e4885d373534267289634d54a677483b8d96f145
ej0306 <54604143+ej0306@users.noreply.github.com>
2023-05-27 14:22:50 -0400

    changed something

--- NewsFeedApp/settings.py
+++ NewsFeedApp/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
 
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR, 'templates'],
 
         'APP_DIRS': True,
         'OPTIONS': {
