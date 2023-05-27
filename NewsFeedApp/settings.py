89d6010e339dd809d01135e2d804e9cec2d351ae
ej0306 <54604143+ej0306@users.noreply.github.com>
2023-05-27 14:40:42 -0400

    changed

--- NewsFeedApp/settings.py
+++ NewsFeedApp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +138,5 @@ STATIC_ROOT = os.path.join(NEWSFEED_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(NEWSFEED_DIR, 'user/static/css'),
 )
+
+django_heroku.settings(locals())
