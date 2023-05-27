c6b3a7f9ef0f537a5d1794f7a28b9e089efd6704
ej0306 <54604143+ej0306@users.noreply.github.com>
2023-05-27 14:54:14 -0400

    made changes

--- NewsFeedApp/settings.py
+++ NewsFeedApp/settings.py
@@ -88,9 +88,11 @@ WSGI_APPLICATION = 'NewsFeedApp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
         'NAME': 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
