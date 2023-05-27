5e8d26c72bb3b6539be97acf616062be76ad1c68
ej0306 <54604143+ej0306@users.noreply.github.com>
2023-05-27 13:42:38 -0400

    made some changes

--- NewsFeedApp/settings.py
+++ NewsFeedApp/settings.py
@@ -123,15 +123,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(NewsFeed_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(NewsFeed_DIR, 'user/static/'),
+)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-STATICFILES_DIRS = (
-    os.path.join(NewsFeed_DIR, 'user/static'),
-)
